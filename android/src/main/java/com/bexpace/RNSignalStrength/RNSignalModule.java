03f82af2fb3dbdae06a09bc675d3758b5b2f3586
Leanid Vouk <bexpace@gmail.com>
2016-10-29 23:28:28 -0700

    added okhttp interceptor

--- android/src/main/java/com/bexpace/RNSignalStrength/RNSignalModule.java
+++ android/src/main/java/com/bexpace/RNSignalStrength/RNSignalModule.java
@@ -4,9 +4,16 @@ import android.support.annotation.Nullable;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.modules.network.OkHttpClientProvider;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by bexp on 10/23/16.
@@ -18,6 +25,21 @@ public class RNSignalModule extends ReactContextBaseJavaModule {
     public RNSignalModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        OkHttpClient client = OkHttpClientProvider.getOkHttpClient();
+        OkHttpClient replacementClient = client
+                .newBuilder()
+                .addNetworkInterceptor(new Interceptor() {
+               @Override
+               public Response intercept(Chain chain) throws IOException {
+                   Request originalRequest = chain.request();
+                   Request requestWithUserAgent = originalRequest.newBuilder()
+                           .addHeader("Hello", "World")
+                           .build();
+                   return chain.proceed(requestWithUserAgent);
+               }
+           }
+        ).build();
+        OkHttpClientProvider.replaceOkHttpClient(replacementClient);
     }
 
     @Override
